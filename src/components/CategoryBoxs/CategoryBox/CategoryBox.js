90caf40915504d1ab6a85534b895b903588a61d5
Daniel O <danielogunlana23@gmail.com>
2021-08-27 11:24:23 -0400

    added router to homepage

--- src/components/CategoryBoxs/CategoryBox/CategoryBox.js
+++ src/components/CategoryBoxs/CategoryBox/CategoryBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classes from "./CategoryBox.module.scss";
-
+import { Link } from "react-router-dom";
 const CategoryBox = (props) => {
   console.log(props);
   return (
-    <div className={classes.CategoryBox}>
+    <Link to={props.category.link} className={classes.CategoryBox}>
       <img src={props.category.imageUrl} alt={props.category.label} />
       <h3>{props.category.label}</h3>
-    </div>
+    </Link>
   );
 };
 
--- src/components/CategoryBoxs/CategoryBoxs.js
+++ src/components/CategoryBoxs/CategoryBoxs.js
@@ -6,21 +6,25 @@ const CategoryBoxs = (props) => {
   const cats = [
     {
       label: "Clubs",
+      link: "/store/clubs/shop-all",
       imageUrl:
         "https://www.directgolf.co.uk/images/marketing/dg-golf-clubs-767x292.jpg",
     },
     {
       label: "Bags",
+      link: "/store/bags/shop-all",
       imageUrl:
         "https://www.directgolf.co.uk/images/marketing/dg-bags-767x292-vers2.jpg",
     },
     {
       label: "Balls",
+      link: "/store/balls/shop-all",
       imageUrl:
         "https://www.directgolf.co.uk/images/marketing/dg-golf-balls-767x292.jpg",
     },
     {
       label: "Shoes",
+      link: "/store/shoes/shop-all",
       imageUrl:
         "https://www.directgolf.co.uk/images/marketing/dg-footwear-767x292.jpg",
     },
